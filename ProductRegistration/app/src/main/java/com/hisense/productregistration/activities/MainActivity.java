c254657ffc5dd2053a688f5d55109dfafca1e814
Tusheet Goli <tusheetgoli@yahoo.com>
2019-07-09 10:12:51 -0400

    location

--- ProductRegistration/app/src/main/java/com/hisense/productregistration/activities/MainActivity.java
+++ ProductRegistration/app/src/main/java/com/hisense/productregistration/activities/MainActivity.java
@@ -10,7 +10,6 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebSettings;
